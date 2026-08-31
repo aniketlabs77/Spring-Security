0ad62d7e11623e4c0ff5619efa4ee40ed1661b34
Aniket <aniketsinghchauhan77@gmail.com>
2026-08-31 10:05:57 +0530

    hh

--- src/main/java/com/aniket/security/SecurityConfig.java
+++ src/main/java/com/aniket/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+	
 	//Authorization
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
